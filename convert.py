221524a7523246f3203d314735ced48edaae9ef5
bob-0727 <70527809+bob-0727@users.noreply.github.com>
2021-10-16 14:36:08 -0500

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,22 @@
+# This script is meant to help users convert the directories in m3u files to thier operating system
+
+# Script by Robert Sherrick
+# Made on October 16, 2021
+# Help from https://www.geeksforgeeks.org/how-to-search-and-replace-text-in-a-file-in-python/
+
+# defining find and replace variables
+search = "/Users/rbs/Music/Music/Media.localized/Music"
+replace = "/home/robert/Music"
+name = input("Enter the playlist name: ")
+
+#function to replace the dirs
+def file(name):
+	with open(name, 'r') as playlist:
+		data = playlist.read()
+		data = data.replace(search, replace)
+
+	with open(name, 'w') as playlist:
+		playlist.write(data)
+
+file(name)
+print("Operation complete!")
